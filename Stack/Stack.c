580575c1b8b70133287dc607b532fbd004b48d39
Dhevin Nandyala <dhevinnandyala@gmail.com>
2022-07-23 21:47:58 +0530

    ,

---
+++ Stack/Stack.c
@@ -0,0 +1,5 @@
+//
+// Created by Dhevin N on 23/7/22.
+//
+
+#include "Stack.h"
---
+++ Stack/Stack.h
@@ -0,0 +1,16 @@
+//
+// Created by Dhevin N on 23/7/22.
+//
+
+#ifndef C_ADTS_STACK_H
+#define C_ADTS_STACK_H
+
+#include "../LinkedList/LinkedList.c"
+
+int peek(LinkedList *this);
+
+int dequeue(LinkedList *this);
+
+int enqueue(LinkedList *this);
+
+#endif //C_ADTS_STACK_H
